5bf2209bd1eb427b0f2f201e77421e3cd5228448
趙子賢 <danny900714@gmail.com>
2022-05-31 19:06:06 +0800

    Add build script and linux amd64 Dockerfile

---
+++ scripts/build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo "building docker images for ${GOOS}/${GOARCH} ..."
+
+REPO="github.com/drone/drone"
+
+# compile the server using the cgo and use nolimit tag
+go build -tags "nolimit" -ldflags "-extldflags \"-static\"" -o release/linux/${GOARCH}/drone-server ${REPO}/cmd/drone-server
