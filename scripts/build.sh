c5ad8fe7adba91b281e22181284a683b550afc1b
趙子賢 <danny900714@gmail.com>
2022-05-31 19:36:21 +0800

    Update build script to fix build failed

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,5 +4,8 @@ echo "building docker images for ${GOOS}/${GOARCH} ..."
 
 REPO="github.com/drone/drone"
 
+# change working directory to drone/
+cd drone
+
 # compile the server using the cgo and use nolimit tag
 go build -tags "nolimit" -ldflags "-extldflags \"-static\"" -o drone/release/linux/${GOARCH}/drone-server ${REPO}/cmd/drone-server
