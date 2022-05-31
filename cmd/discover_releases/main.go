4ec4ce610fbd4b664a51af96b27153dc40fb51f0
趙子賢 <danny900714@gmail.com>
2022-05-31 22:27:36 +0800

    Add event and duration support in discover releases

--- cmd/discover_releases/main.go
+++ cmd/discover_releases/main.go
@@ -10,17 +10,27 @@ import (
 )
 
 func main() {
+	event := os.Getenv("DRONE_BUILD_EVENT")
+	duration, err := time.ParseDuration(os.Getenv("DURATION"))
+	if err != nil {
+		panic(err)
+	}
+
 	client := github.NewClient(nil)
 	releases, _, _ := client.Repositories.ListReleases(context.Background(), "drone", "drone", nil)
 
 	// Get tag names
 	var tagNames []string
 	for _, release := range releases {
-		duration := time.Since(release.PublishedAt.Time)
-		hours := duration.Hours()
-		if hours < 384 && !*release.Prerelease {
+		releaseDuration := time.Since(release.PublishedAt.Time)
+		if releaseDuration < duration && !*release.Prerelease {
 			tagNames = append(tagNames, *release.TagName)
 		}
+	}
+
+	// retain last tag if event isn't cron and tagNames is empty
+	if len(tagNames) == 0 && event != "cron" {
+		tagNames = append(tagNames, *releases[0].TagName)
 	}
 
 	// Write tag name to tags.txt
